ebda73e9666eec0cb7c867960daf8959b51431e0
Herb Hagely <herb.hagely@gmail.com>
2023-11-18 17:26:05 -0600

    add ability to select a race session; working on displaying driver info

--- f1App/races/urls.py
+++ f1App/races/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('about/', views.about, name="about"),
     # path("/races/", views.race_schedule, name="race_schedule"),
-    path("<str:event_name>/", views.detail, name="detail")
+    path("<str:event_name>/", views.detail, name="detail"),
+    path("<str:event_name>/<str:session>/", views.race_session, name="race_session")
 ]
--- f1App/races/views.py
+++ f1App/races/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import fastf1
 
 
@@ -25,6 +24,42 @@ def detail(request, event_name):
   schedule = fastf1.get_event_schedule(2023)
   event = schedule.get_event_by_name(event_name)
 
+  context = {
+    "event_name": event['OfficialEventName'],
+    "session_list": [
+      event['Session1'],
+      event['Session2'],
+      event['Session3'],
+      event['Session4'],
+      event['Session5'],
+    ]
+  }
+
+
   print(event)
 
-  return render(request, "races/detail.html", context = { "event": event })
+  return render(request, "races/detail.html", context=context)
+
+def race_session(request, event_name, session):
+  sesh = fastf1.get_session(2023, event_name, session)
+  sesh.load()
+
+  results = sesh.results
+
+  print(results['q2'])
+
+  session_data = list(zip(
+    results['DriverNumber'],
+    results['Abbreviation'],
+    results['Position'],
+    results['Q1'],
+    # results['Q2'],
+    # results['Q3']
+  ))
+
+  print(sesh.results)
+
+  return render(request, "races/race_session.html", context={"session_info": session_data})
+
+# def load_drivers(request, event_name):
+#   return render(request, "races/load_drivers.html")
