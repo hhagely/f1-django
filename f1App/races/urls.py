de3ef541fe70cc4665b0a55729d975fe3880279a
Herb Hagely <herb.hagely@gmail.com>
2023-11-21 13:32:30 -0600

    start handling situational session data; create and display driver lap times for a race

--- f1App/races/urls.py
+++ f1App/races/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('about/', views.about, name="about"),
-    # path("/races/", views.race_schedule, name="race_schedule"),
     path("<str:event_name>/", views.detail, name="detail"),
-    path("<str:event_name>/<str:session>/", views.race_session, name="race_session")
+    path("<str:event_name>/<str:session>/", views.race_session, name="race_session"),
+    path("<str:event_name>/<str:session_type>/<str:driver_abbr>/", views.driver_laps, name="driver_laps"),
 ]
--- f1App/races/views.py
+++ f1App/races/views.py
@@ -21,6 +21,8 @@ def detail(request, event_name):
   schedule = fastf1.get_event_schedule(2023)
   event = schedule.get_event_by_name(event_name)
 
+  print(event)
+
   context = {
     "event_name": event['OfficialEventName'],
     "session_list": [
@@ -34,11 +36,30 @@ def detail(request, event_name):
 
   return render(request, "races/detail.html", context=context)
 
+# def qualifying(request, event_name, session):
+#   sesh = fastf1.get_session(2023, event_name, session)
+#   sesh.load()
+
+#   results = sesh.results
+
+#   session_data = list(zip(
+#     results['DriverNumber'],
+#     results['Abbreviation'],
+#     results['Position'],
+#     results['Q1'].fillna('Eliminated'),
+#     results['Q2'].fillna('Eliminated'),
+#     results['Q3'].fillna('Eliminated')
+#   ))
+
+#   return render(request, "races/qualifying.html", context={"session_info": session_data})
+
 def race_session(request, event_name, session):
   sesh = fastf1.get_session(2023, event_name, session)
   sesh.load()
 
   results = sesh.results
+
+  print(results)
 
   session_data = list(zip(
     results['DriverNumber'],
@@ -49,7 +70,72 @@ def race_session(request, event_name, session):
     results['Q3'].fillna('Eliminated')
   ))
 
-  return render(request, "races/race_session.html", context={"session_info": session_data})
+  # todo: need to modify the session data based on the session type
+  # todo: ex: race session data should not have q1, q2, q3 columns on it
+  match session:
+    case "Practice 1":
+      template = "races/practice.html"
+    case "Practice 2":
+      template = "races/practice.html"
+    case "Practice 3":
+      template = "races/practice.html"
+    case "Qualifying":
+      template = "races/qualifying.html"
+      session_data = list(zip(
+        results['DriverNumber'],
+        results['Abbreviation'],
+        results['Position'],
+        results['Q1'].fillna('Eliminated'),
+        results['Q2'].fillna('Eliminated'),
+        results['Q3'].fillna('Eliminated')
+      ))
+    case "Sprint Shootout":
+      template = "races/qualifying.html"
+      session_data = list(zip(
+        results['DriverNumber'],
+        results['Abbreviation'],
+        results['Position'],
+        results['Q1'].fillna('Eliminated'),
+        results['Q2'].fillna('Eliminated'),
+        results['Q3'].fillna('Eliminated')
+      ))
+    case _:
+      template = "races/race_session.html"
+      session_data = list(zip(
+        results['DriverNumber'],
+        results['Abbreviation'],
+        results['ClassifiedPosition'],
+        results['GridPosition'],
+        results['Status'],
+        results['Points'],
+      ))
 
-# def load_drivers(request, event_name):
-#   return render(request, "races/load_drivers.html")
+
+  return render(request, template, context={
+    "event_name": event_name,
+    "session_type": session,
+    "session_info": session_data,
+    }
+  )
+
+def driver_laps(request, event_name, session_type, driver_abbr):
+  session = fastf1.get_session(2023, event_name, session_type)
+
+  session.load()
+
+  driver_lap_info =  session.laps.pick_driver(driver_abbr)
+
+  lap_info = list(zip(
+    driver_lap_info['LapNumber'], # todo: convert to int
+    driver_lap_info['Stint'],
+    driver_lap_info['LapTime'],
+    driver_lap_info['Position'],
+    driver_lap_info['Deleted'],
+    driver_lap_info['DeletedReason'],
+  ))
+
+  return render(request, "races/driver_laps.html", context={
+    "lap_info": lap_info,
+    "driver_abbr": driver_abbr,
+    "event_name": event_name
+  })
